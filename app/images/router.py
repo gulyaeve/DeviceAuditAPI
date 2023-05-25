b586a96c46e045305303b675246fa07b4d1f58db
gulyaeve <gulyaev.e@icloud.com>
2023-05-25 19:52:57 +0300

    Added image resized

--- app/images/router.py
+++ app/images/router.py
@@ -1,6 +1,8 @@
 from fastapi import APIRouter, UploadFile
 import shutil
 
+from app.tasks.tasks import process_pic
+
 router = APIRouter(prefix="/images", tags=["Загрузка картинок"])
 
 
@@ -10,6 +12,7 @@ router = APIRouter(prefix="/images", tags=["Загрузка картинок"])
     description="Загрузка фото устройства с идентификатором исследования",
 )
 async def add_device_image(device_id: int, inspection_id: int, file: UploadFile):
-    im_path = f"app/static/images/{device_id}_{inspection_id}.webp"
+    im_path = f"app/static/images/{device_id}_{inspection_id}.png"
     with open(im_path, "wb+") as file_object:
         shutil.copyfileobj(file.file, file_object)
+    process_pic.delay(im_path)
--- app/tasks/tasks.py
+++ app/tasks/tasks.py
@@ -3,13 +3,11 @@ from PIL import Image
 from pathlib import Path
 
 
-# @celery.task
-# def process_pic(
-#     path: str,
-# ):
-#     im_path = Path(path)
-#     im = Image.open(im_path)
-#     im_resized_1000_500 = im.resize((1000, 500))
-#     im_resized_200_100 = im.resize((200, 100))
-#     im_resized_1000_500.save(f"app/static/images/resized_1000_500_{im_path.name}.webp")
-#     im_resized_200_100.save(f"app/static/images/resized_200_100_{im_path.name}.webp")
+@celery.task
+def process_pic(
+    path: str,
+):
+    im_path = Path(path)
+    im = Image.open(im_path)
+    im.thumbnail((600, 600), Image.Resampling.LANCZOS)
+    im.save(f"app/static/images/resized_{im_path.name}.png")

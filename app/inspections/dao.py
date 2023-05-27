ee5ca4cd93f0765345aaa7d6fb9910bcb393420f
gulyaeve <gulyaev.e@icloud.com>
2023-05-27 19:52:22 +0300

    Added validation to reference

--- app/inspections/dao.py
+++ app/inspections/dao.py
@@ -1,3 +1,4 @@
+import re
 
 from pydantic import json
 from sqlalchemy import insert
@@ -6,8 +7,10 @@ from app.dao.base import BaseDAO
 from app.database import async_session_maker
 from app.devices.dao import DevicesDAO
 from app.devices.tech_specs.dao import TechSpecsDAO
+from app.devices.tech_specs.models import TechSpecs
 from app.exceptions import EntityNotExistsException, InspectionValidateException
 from app.inspections.models import Inspections
+from app.inspections.utils import validate_to_reference
 from app.logger import logger
 
 
@@ -18,9 +21,21 @@ class InspectionsDAO(BaseDAO):
     async def add(cls, device_id: int, image_path: str, data: json):
         device = await DevicesDAO.find_one_or_none(id=device_id)
         if device:
-            tech_specs = await TechSpecsDAO.find_all(device_id=device_id)
+            tech_specs: list[TechSpecs] = await TechSpecsDAO.find_all(device_id=device_id)
             tech_specs_list = [tech_spec.description for tech_spec in tech_specs]
             if tech_specs_list == list(data.keys()):
+
+                for index, (key, value) in enumerate(data.items()):
+                    reference = tech_specs[index].reference_value
+                    validation = validate_to_reference(value, reference)
+                    if validation is not None:
+                        if validation is True:
+                            data[key] += f" ({reference}) Соответствует стандарту"
+                        else:
+                            data[key] += f" ({reference}) Не соответствует стандарту"
+                    else:
+                        pass
+
                 async with async_session_maker() as session:
                     query = (
                         insert(cls.model)
---
+++ app/inspections/utils.py
@@ -0,0 +1,28 @@
+import re
+
+
+def validate_to_reference(value, reference: str):
+    if reference == "Да":
+        if value.lower() == "да":
+            result = True
+        else:
+            result = False
+    elif reference.startswith(">="):
+        if int(value) >= int(re.findall(r'\d+', reference)[0]):
+            result = True
+        else:
+            result = False
+    elif reference.startswith("<="):
+        if int(value) <= int(re.findall(r'\d+', reference)[0]):
+            result = True
+        else:
+            result = False
+    elif len(re.findall(r'\d+', reference)) == 2:
+        if int(re.findall(r'\d+', reference)[0]) <= int(value) <= int(re.findall(r'\d+', reference)[1]):
+            result = True
+        else:
+            result = False
+    else:
+        result = None
+    return result
+

e159000614459b482bf0c8d968448d57fd2986ba
gulyaeve <gulyaev.e@icloud.com>
2023-06-07 16:07:10 +0300

    Added import tech specs from csv

--- app/importer/router.py
+++ app/importer/router.py
@@ -3,11 +3,10 @@ import shutil
 
 from fastapi import APIRouter, UploadFile, Depends
 
+from app.auth.scheme import get_token
 from app.config import settings
-from app.exceptions import UserIsNotPresentException
 from app.importer.utils import import_from_csv
-from app.users.dependencies import get_current_user
-from app.users.models import Users
+from app.logger import logger
 
 router = APIRouter(
     prefix="/import",
@@ -17,12 +16,11 @@ router = APIRouter(
 
 @router.post("/{table_name}", status_code=201)
 async def import_csv(
-    table_name: str, file: UploadFile, user: Users = Depends(get_current_user)
+    table_name: str, file: UploadFile, token: str = Depends(get_token)
 ):
-    if user:
-        file_path = f"{settings.STATIC_DIR}/csv/{table_name}_{datetime.datetime.now()}.csv"
-        with open(file_path, "wb+") as file_object:
-            shutil.copyfileobj(file.file, file_object)
-        await import_from_csv(table_name, file_path)
-    else:
-        raise UserIsNotPresentException
+    logger.info(f"Import {table_name}", extra={"token": token})
+    file_path = f"{settings.STATIC_DIR}/csv/{table_name}_{datetime.datetime.now()}.csv"
+    with open(file_path, "wb+") as file_object:
+        shutil.copyfileobj(file.file, file_object)
+    await import_from_csv(table_name, file_path)
+
